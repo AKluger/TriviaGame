1a39598778d06a4d2515287f4eb797999862a721
Alex <aikluger@gmail.com>
2018-11-30 23:39:26 -0500

    PseudoCode body outline and script

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+
+//start main function to run on window open
+
+
+//use click event for start button to call the first question 
+
+//Use element Ids and jQuery to label answer divs, 4 per question with click.
+//also use hover event on each answer div
+
+//use stopwatch functionality or set TimeOut?  including reset function to be called for each question page, putting starting time in clock div to begin countdown.  
+//Also include count function but starting from 25 seconds.
+//And StopInterval function
+
+//if timer runs out...
+
+//setTimeout for answer reveal page that calls function of  next question
+
+
+
+
+
+
+
+
+
+//scoreboard object featuring correct incorrect and unanswered to be entered in answer divs on final page
+
